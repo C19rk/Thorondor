e4fbf01e6a6394ee7b1c8b5f5e7d996747a84c28
C19rk <manuntagclark@gmail.com>
2026-01-11 15:13:21 +0800

    Changed folder names into a much more suitable name

--- system/core/config.py
+++ system/core/config.py
@@ -6,9 +6,9 @@ CSV_FILE = "detections.csv"
 CAMERA_SOURCES = {
     # "cam1: "rtsp://username:password@tapo_ip_address:554/stream1", (strem 1 for hd 2 for low)"
     # Connect to the same 2.4 GHz network as the camera
-    "cam1": "rtsp://camera1:camera1234@192.168.254.112:554/stream2",
-    "cam2": "rtsp://camera1:camera1234@192.168.254.112:554/stream2",
-    "cam3": "rtsp://camera1:camera1234@192.168.254.112:554/stream2",
+    "cam1": "rtsp://camera1:camera1234@192.168.254.111:554/stream2",
+    "cam2": "rtsp://camera1:camera1234@192.168.254.111:554/stream2",
+    "cam3": "rtsp://camera1:camera1234@192.168.254.111:554/stream2",
 }
 
 # --- resolution settings ---
@@ -16,9 +16,9 @@ FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 
 # === MODELS ===
-YOLO_MODEL_PATH = "../App/runs/aidetection7/weights/best.pt"
-YOLO_DESK_MODEL_PATH = "../App/runs/aidetectiondesk2/weights/best.pt"
-POSE_MODEL_PATH = "../App/yolo11n-pose.pt"
+YOLO_MODEL_PATH = "../machine_learning/runs/aidetection7/weights/best.pt"
+YOLO_DESK_MODEL_PATH = "../machine_learning/runs/aidetectiondesk2/weights/best.pt"
+POSE_MODEL_PATH = "../machine_learning/yolo11n-pose.pt"
 
 # === THRESHOLDS ===
 YOLO_CONF_THRESHOLD = 0.75
